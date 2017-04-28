d18d1a08e9bdecd82783b73736c68a1a1b9063d8
The sample project - Tecnology <dennix.apk@gmail.com>
2017-04-28 07:40:06 +0700

    sek

--- PBO/src/com/modul4/soal/DaftarBuku.java
+++ PBO/src/com/modul4/soal/DaftarBuku.java
@@ -1,5 +1,6 @@
 package com.modul4.soal;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class DaftarBuku {
@@ -8,8 +9,8 @@ public class DaftarBuku {
 		Scanner scan = new Scanner(System.in);	
 		int infoBuku;
 		System.out.println("Masukkan jumlah buku yang akan di input : ");
-		infoBuku = scan.nextInt();
-		KelolaBuku[] kelolaBuku = new KelolaBuku[infoBuku];
+		//infoBuku = scan.nextInt();
+		KelolaBuku[] kelolaBuku = new KelolaBuku[6];
 		
 		String ya;
 		boolean ulangi = false;
@@ -31,12 +32,19 @@ public class DaftarBuku {
 					ulangi = true;
 				i++;
 			} while (ulangi);
-		}catch (Exception e) {
+		}catch (InputMismatchException e) {
+			// TODO: handle exception
+			System.out.println("Masukkan data dengan benar!");
+		}catch (ArrayIndexOutOfBoundsException e) {
+			// TODO: handle exception
+			System.out.println("Data yang anda masukkan melebihi batas!");
+		}
+		catch (Exception e) {
 			// TODO: handle exception
 			System.out.println(e);
 			
 		}
-		for(int n = 0; n < infoBuku; n++){
+		for(int n = 0; n < 6; n++){
 			if(kelolaBuku[n].getJudulBuku() != null){
 				System.out.println("buku ke : "+n);
 				System.out.println("Judul buku : "+kelolaBuku[n].getJudulBuku());
